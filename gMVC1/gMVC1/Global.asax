3fa9994f4dd7a17b0fba64168fa0e42e21ab383d
Max Sun <sggmadax@gmail.com.com>
2017-09-22 09:59:28 +0800

    this is my first commit to github from vs2015

---
+++ gMVC1/gMVC1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gMVC1.MvcApplication" Language="C#" %>
---
+++ gMVC1/gMVC1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gMVC1.Startup))]
+namespace gMVC1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
